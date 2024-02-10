bbc0e1060e2b062aa43128e72d18fcb606fbeb64
u61081 <u61081@intersvyaz.net>
2024-02-10 16:54:03 +0500

    added bootstrap

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,15 +3,16 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts() {
         return List.of(
-                new Post("The majestic lion roams freely across the African savannah, exuding strength and elegance."),
-                new Post("The playful dolphins leap and spin in the crystal-clear waters, delighting onlookers with their acrobatics."),
-                new Post("The agile cheetah, known as the fastest land animal, gracefully hunts its prey on the vast plains.")
+                new Post("The majestic lion roams freely across the African savannah, exuding strength and elegance.", new Date()),
+                new Post("The playful dolphins leap and spin in the crystal-clear waters, delighting onlookers with their acrobatics.", new Date()),
+                new Post("The agile cheetah, known as the fastest land animal, gracefully hunts its prey on the vast plains.", new Date())
         );
     }
 }
